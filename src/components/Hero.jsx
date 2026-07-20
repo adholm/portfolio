70079700da17727f97b13d69f8d71a699677e024
Adam Holm <holmadam@outlook.com>
2026-07-20 15:03:03 +0200

    yaml identation fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
                     Adam Holm
                 </h1>
                 <p className="mt-1 text-lg font-semibold text-gray-300 drop-shadow-md">
-                    Tech Specialist with a passion for Linux, back-end, UI/UX & Cybersecurity. Based in Stockholm.
+                    Tech Specialist with a passion for Linux, backend, UI/UX & Cybersecurity. Based in Stockholm. TEST
                 </p>
             </div>
 

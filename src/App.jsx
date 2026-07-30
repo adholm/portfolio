6e296e071791ed2950155164433abc07ad4f7f4b
Adam Holm <holmadam@outlook.com>
2026-07-30 18:23:34 +0200

    mobile scaling hero adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,28 @@ import AboutPage from "./pages/AboutPage";
 function App() {
   return (
     <Router>
+    <div className="relative flex flex-col min-h-screen text-white bg-slate-950">
+
+    {/* FIXERAD BAKGRUND: Löser mobil-zoomen och ger perfekt passform */}
     <div
-    className="flex flex-col min-h-screen text-white bg-cover bg-center bg-fixed bg-no-repeat"
-    style={{ backgroundImage: "url('https://images.unsplash.com/photo-1651497111372-9107c1a5d9a0')" }}
-    >
+    className="fixed inset-0 z-0 bg-cover bg-center bg-no-repeat pointer-events-none"
+    style={{
+      backgroundImage: "url('https://images.unsplash.com/photo-1651497111372-9107c1a5d9a0')",
+    }}
+    />
+
+    {/* INNEHÅLL (Ligger ovanpå bakgrunden med z-10) */}
+    <div className="relative z-10 flex flex-col min-h-screen">
     <Navbar />
 
     <main className="flex-grow flex flex-col">
     <Routes>
-    {/* HEM-SIDAN: Perfekt centrerad Hero på mobil med 75vh höjd för synlig bakgrund */}
+    {/* HEM-SIDAN */}
     <Route path="/" element={
       <div className="flex-1 flex flex-col justify-between w-full pt-16 md:pt-0">
-
-      {/* Övre zon: Tvingar fram 75% av skärmhöjden på mobil så bakgrunden syns ordentligt */}
       <div className="flex-1 flex items-center justify-center min-h-[75vh] md:min-h-0 my-auto">
       <Hero />
       </div>
-
-      {/* Nedre zon: Skjuts ner under "first fold" på mobil, dockad mot botten på desktop */}
       <Projects />
       </div>
     } />
@@ -44,6 +48,7 @@ function App() {
     </main>
 
     <Footer />
+    </div>
     </div>
     </Router>
   );

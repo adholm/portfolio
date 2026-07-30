2741832dc2be9020c497a63e30ab888b49ad8ab7
Adam Holm <holmadam@outlook.com>
2026-07-30 20:57:30 +0200

    pfp

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,11 @@ const Hero = () => {
         <section className="w-full relative flex flex-col items-center justify-center py-0">
         <div className="relative z-10 max-w-4xl mx-auto text-center px-4 flex flex-col items-center">
 
-        {/* 1. Profilbild (Större på mobil, uppflyttad) */}
+        {/* 1. Profilbild (Större på mobil, uppflyttad) test */}
         <div className="relative mb-4 sm:mb-5 -mt-12 sm:mt-0 group">
         <div className="absolute -inset-1 bg-gradient-to-r from-teal-500/30 to-emerald-500/30 rounded-full blur-lg opacity-30 group-hover:opacity-60 transition duration-500"></div>
         <img
-        src="/images/pfp.jpeg"
+        src="/images/pfp5.png"
         alt="Adam Holm"
         className="relative w-40 h-40 sm:w-44 sm:h-44 md:w-48 md:h-48 rounded-full object-cover border-2 border-white/20 shadow-2xl transition-transform duration-300 group-hover:scale-105"
         />
